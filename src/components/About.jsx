d372a9c3bf1b5dd1a098a445b169cdc44801cfe7
itsriyas <mohamedriyas.dev@gmail.com>
2025-07-18 16:29:25 +0530

    fixes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,10 +10,10 @@ const AboutSection = () => {
         <Row className="align-items-center">
           {/* Text Content */}
           <Col md={6} data-aos="fade-right">
-            <p>
+            <p className='text-center'>
               At <strong>Blue Mario Events</strong>, we don't just plan events we craft unforgettable experiences. Whether it's a dream wedding, corporate gala, or themed celebration, our team brings creativity, professionalism, and passion to every moment.
             </p>
-            <p>
+            <p className='text-center'>
               From concept to execution, we handle every detail meticulously venue selection, decor, entertainment, and guest management. Our goal is to make your event stress-free and stunning, reflecting your vision and personality.
             </p>
             <p className="mb-0">
